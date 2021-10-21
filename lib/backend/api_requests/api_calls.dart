9123ca96a8bd42e1c48b731de3341ff05a9994aa
Yuji Takase <yuji.takase@particledrawing.com>
2021-10-21 17:15:20 +0900

    Update the API url

--- lib/backend/api_requests/api_calls.dart
+++ lib/backend/api_requests/api_calls.dart
@@ -47,7 +47,7 @@ Future<dynamic> registContentsCall({
 }''';
   return ApiManager.instance.makeApiCall(
     callName: 'Regist Contents',
-    apiUrl: 'https://asia-northeast1-baylifedev.cloudfunctions.net/sendMailV1',
+    apiUrl: 'https://asia-northeast1-baylife-ff782.cloudfunctions.net/sendMailV1',
     callType: ApiCallType.POST,
     headers: {
       'Content-Type': 'application/json',
