7cfab730d20dfc44c2b926467628e366f0b72fe9
Yuji Takase <yuji.takase@particledrawing.com>
2022-05-23 10:57:29 +0900

    Update the size of the container on infinite scroll list

--- lib/order_list_page/order_list_page_widget.dart
+++ lib/order_list_page/order_list_page_widget.dart
@@ -323,7 +323,7 @@ class _OrderListPageWidgetState extends State<OrderListPageWidget> {
                                     width: MediaQuery.of(context).size.width *
                                         0.72,
                                     height: MediaQuery.of(context).size.height *
-                                        0.64,
+                                        0.72,
                                     decoration: BoxDecoration(
                                       color: FlutterFlowTheme.of(context)
                                           .background,
