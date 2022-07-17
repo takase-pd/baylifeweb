2d6d8abed4269d4d4940ca3ef8830bf14014fe44
Yuji Takase <yuji.takase@particledrawing.com>
2022-07-18 06:56:45 +0900

    Update to add include tax and verify age

--- lib/components/update_plan_page_widget.dart
+++ lib/components/update_plan_page_widget.dart
@@ -38,6 +38,7 @@ class _UpdatePlanPageWidgetState extends State<UpdatePlanPageWidget> {
   TextEditingController textController8;
   TextEditingController textController9;
   bool radioButtonValue;
+  bool switchListTileValue;
   String initShipping;
   DocumentReference shopRef;
   final formKey = GlobalKey<FormState>();
@@ -414,6 +415,73 @@ class _UpdatePlanPageWidgetState extends State<UpdatePlanPageWidget> {
                                 mainAxisSize: MainAxisSize.max,
                                 children: [
                                   Expanded(
+                                    flex: 5,
+                                    child: Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          16, 0, 0, 0),
+                                      child: TextFormField(
+                                        initialValue:
+                                            '酒類など20才以上の方のみ注文できる商品の場合スイッチをオン',
+                                        readOnly: true,
+                                        obscureText: false,
+                                        decoration: InputDecoration(
+                                          labelText: '年齢制限商品',
+                                          enabledBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: Color(0x00000000),
+                                              width: 1,
+                                            ),
+                                            borderRadius:
+                                                BorderRadius.circular(4),
+                                          ),
+                                          focusedBorder: UnderlineInputBorder(
+                                            borderSide: BorderSide(
+                                              color: Color(0x00000000),
+                                              width: 1,
+                                            ),
+                                            borderRadius:
+                                                BorderRadius.circular(4),
+                                          ),
+                                        ),
+                                        style: FlutterFlowTheme.of(context)
+                                            .bodyText2
+                                            .override(
+                                              fontFamily: 'Montserrat',
+                                              color:
+                                                  FlutterFlowTheme.of(context)
+                                                      .textDark,
+                                              fontWeight: FontWeight.w500,
+                                            ),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: SwitchListTile(
+                                      value: switchListTileValue ??= false,
+                                      onChanged: (newValue) => setState(
+                                          () => switchListTileValue = newValue),
+                                      dense: false,
+                                      controlAffinity:
+                                          ListTileControlAffinity.trailing,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(0, 0, 0, 16),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: FlutterFlowTheme.of(context).background,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Expanded(
                                     child: Padding(
                                       padding: EdgeInsetsDirectional.fromSTEB(
                                           16, 0, 16, 8),
@@ -433,7 +501,7 @@ class _UpdatePlanPageWidgetState extends State<UpdatePlanPageWidget> {
                                         },
                                         obscureText: false,
                                         decoration: InputDecoration(
-                                          labelText: '料金（半角数字）',
+                                          labelText: '料金（半角数字、税込額を入力）',
                                           isDense: true,
                                           enabledBorder: UnderlineInputBorder(
                                             borderSide: BorderSide(
@@ -1126,7 +1194,7 @@ class _UpdatePlanPageWidgetState extends State<UpdatePlanPageWidget> {
                                         },
                                         obscureText: false,
                                         decoration: InputDecoration(
-                                          labelText: '送料（半角数字）',
+                                          labelText: '送料（半角数字、税込額を入力）',
                                           isDense: true,
                                           enabledBorder: UnderlineInputBorder(
                                             borderSide: BorderSide(
